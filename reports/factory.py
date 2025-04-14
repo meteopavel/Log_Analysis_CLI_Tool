a2bf697a2b4140ad2c3abaebba768e9eed9caa4c
meteopavel <isu@meteopavel.ru>
2025-04-14 16:29:54 +0800

    feat(reports): add base classes for reports

--- reports/factory.py
+++ reports/factory.py
@@ -4,7 +4,7 @@ from config.constants import REPORT_TYPES
 
 class ReportFactory:
     @staticmethod
-    def create_report(report_type: str):
+    def create_report(report_type):
         if report_type not in REPORT_TYPES:
             raise ValueError(f'Unknown report type: {report_type}')
         module_path, class_name = REPORT_TYPES[report_type].rsplit('.', 1)
