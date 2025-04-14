1463aa08280b1ba4c0dc086a03124c3a6e2ee37a
meteopavel <isu@meteopavel.ru>
2025-04-14 17:14:52 +0800

    refactor: remove unnecessary implementations and update README

--- config/constants.py
+++ config/constants.py
@@ -2,8 +2,9 @@ import re
 
 REPORT_TYPES = {
     'handlers': 'reports.registry.HandlersReport',
-    'management': 'reports.registry.ManagementReport',
-    'backends': 'reports.registry.BackendsReport',
+    # Раскомментируйте для добавления других типов отчета:
+    # 'management': 'reports.registry.ManagementReport',
+    # 'backends': 'reports.registry.BackendsReport',
 }
 
 LOG_PATTERNS = {
@@ -13,18 +14,20 @@ LOG_PATTERNS = {
         r'django\.request:\s+'
         r'.*?(?P<handler>/[^\s\]]+)'
     ),
-    'management': re.compile(
-        r'^(?P<timestamp>.+?)\s+'
-        r'(?P<level>\w+)\s+'
-        r'django\.core\.management:\s+'
-        r'.*?(?P<message>.+)'
-    ),
-    'backends': re.compile(
-        r'^(?P<timestamp>.+?)\s+'
-        r'(?P<level>\w+)\s+'
-        r'django\.db\.backends:\s+'
-        r'.*?(?P<query>.+)'
-    ),
+    # Раскомментируйте для добавления регулярного выражения для других
+    # типов отчета:
+    # 'management': re.compile(
+    #     r'^(?P<timestamp>.+?)\s+'
+    #     r'(?P<level>\w+)\s+'
+    #     r'django\.core\.management:\s+'
+    #     r'.*?(?P<message>.+)'
+    # ),
+    # 'backends': re.compile(
+    #     r'^(?P<timestamp>.+?)\s+'
+    #     r'(?P<level>\w+)\s+'
+    #     r'django\.db\.backends:\s+'
+    #     r'.*?(?P<query>.+)'
+    # ),
 }
 
 LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
--- reports/registry.py
+++ reports/registry.py
@@ -6,11 +6,17 @@ class HandlersReport(GenericReport):
         super().__init__(report_type='handlers', header_name='HANDLER')
 
 
-class ManagementReport(GenericReport):
-    def __init__(self):
-        super().__init__(report_type='management', header_name='MESSAGE')
+# Раскомментируйте эти классы чтобы добавить другие виды отчетов
+# class ManagementReport(GenericReport):
+#     def __init__(self):
+#         super().__init__(report_type='management', header_name='MESSAGE')
 
 
-class BackendsReport(GenericReport):
-    def __init__(self):
-        super().__init__(report_type='backends', header_name='QUERY')
+# class BackendsReport(GenericReport):
+#     def __init__(self):
+#         super().__init__(report_type='backends', header_name='QUERY')
+
+
+# class NewReport(GenericReport):
+#     def __init__(self):
+#         super().__init__(report_type='new_report', header_name='NEW')
